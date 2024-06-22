8b1b80249a7aa8f60053ab204a43c977750cd18c
Michael Adeyemo <michaeladesola1410@gmail.com>
2024-06-22 13:15:13 +0100

    Added create entry test

--- db/sqlc/accounts.sql_test.go
+++ db/sqlc/accounts.sql_test.go
@@ -6,17 +6,20 @@ import (
 	"testing"
 
 	"github.com/morgan/simplebank/utils"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-var createParams = CreateAccountParams{
-	Owner:    utils.RandomName(),
-	Balance:  utils.RandomMoney(),
-	Currency: "NGN",
+func createParams() CreateAccountParams {
+	return CreateAccountParams{
+		Owner:    utils.RandomName(),
+		Balance:  utils.RandomMoney(),
+		Currency: "NGN",
+	}
 }
 
 func createTestAccount() (acct Account) {
-	acct, err := testQueries.CreateAccount(context.Background(), createParams)
+	acct, err := testQueries.CreateAccount(context.Background(), createParams())
 	if err != nil {
 		log.Fatal("Error occured when creating account", err)
 	}
@@ -24,10 +27,46 @@ func createTestAccount() (acct Account) {
 }
 func TestCreateAccount(t *testing.T) {
 	acct := createTestAccount()
-	require.Equal(t, acct.Owner, createParams.Owner)
-	require.Equal(t, acct.Balance, createParams.Balance)
-	require.Equal(t, acct.Currency, createParams.Currency)
 	require.NotZero(t, acct.ID)
 	require.NotZero(t, acct.CreatedAt)
 
+}
+
+func TestDeleteAccount(t *testing.T) {
+	acct := createTestAccount()
+	require.NotEmpty(t, acct)
+	err := testQueries.DeleteAccount(context.Background(), acct.ID)
+	require.NoError(t, err)
+	acct2, err := testQueries.GetAccount(context.Background(), acct.ID)
+	require.Error(t, err)
+	require.Empty(t, acct2)
+}
+
+func TestGetAccount(t *testing.T) {
+	acct := createTestAccount()
+	require.NotEmpty(t, acct)
+	gAcct, err := testQueries.GetAccount(context.Background(), acct.ID)
+	require.NoError(t, err)
+	assert.Equal(t, acct, gAcct)
+}
+
+func TestListAccount(t *testing.T) {
+	acct := createTestAccount()
+	require.NotEmpty(t, acct)
+	accts, err := testQueries.ListAccount(context.Background(), ListAccountParams{1, 1})
+	require.NoError(t, err)
+	assert.NotEmpty(t, accts)
+	require.Len(t, accts, 1)
+
+}
+
+func TestUpdateAccount(t *testing.T) {
+	acct := createTestAccount()
+	args := UpdateAccountParams{
+		ID: acct.ID, Balance: utils.RandomMoney(),
+	}
+	acct, err := testQueries.UpdateAccount(context.Background(), args)
+	require.NoError(t, err)
+	assert.NotEmpty(t, acct)
+	assert.Equal(t, acct.Balance, args.Balance)
 }
